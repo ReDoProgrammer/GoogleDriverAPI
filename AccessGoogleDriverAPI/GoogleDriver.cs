5efab4b8365125969d41267242b8b6d607c25deb
ReDoProgrammer <redoprogrammer@gmail.com>
2021-06-27 21:54:36 +0700

    inital google driver api

---
+++ AccessGoogleDriverAPI/GoogleDriver.cs
@@ -0,0 +1,62 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessGoogleDriverAPI
+{
+
+    public class GoogleDriver
+    {
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string ApplicationName = "Google Driver";
+        public UserCredential credential;
+        public GoogleDriver()
+        {
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+        }
+
+        public IList<Google.Apis.Drive.v3.Data.File> ListFile()
+        {
+            try
+            {
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                FilesResource.ListRequest listRequest = service.Files.List();
+                listRequest.PageSize = 10;
+                listRequest.Fields = "nextPageToken, files(id, name)";
+                return listRequest.Execute().Files;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }      
+        }
+    }
+}
